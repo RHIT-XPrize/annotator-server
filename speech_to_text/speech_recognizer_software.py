49a66d360f00a645da689c6b316c7eee44b19740
crowelmk <crowelmk@rose-hulman.edu>
2019-02-06 00:38:16 -0500

    Made errors not fail the app

--- speech_to_text/speech_recognizer_software.py
+++ speech_to_text/speech_recognizer_software.py
@@ -21,9 +21,11 @@ class GoogleCloudSpeechConverter(SpeechConverter):
             return processed_text
         except sr.UnknownValueError:
             print("Google Cloud Speech could not understand audio")
-            raise sr.UnknownValueError
+            print(sr.UnknownValueError)
+            return ""
         except sr.RequestError as e:
             print("Could not request results from Google Cloud Speech service; {0}".format(e))
-            raise sr.RequestError
+            print(sr.RequestError)
+            return ""
         
 
